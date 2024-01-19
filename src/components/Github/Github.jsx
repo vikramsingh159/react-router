a355f116b560a024a442424f032d3d684e354d4b
Vikram Singh <er.vikramsingh10@gmail.com>
2024-01-19 19:26:28 +0530

    my git repo use and static type only for using react router dom v6 useing and outle layout loader etc

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,17 @@
+import { useLoaderData } from 'react-router-dom';
+
+const Github = () => {
+    const data = useLoaderData()
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+            <img src={data.avatar_url} alt="Git picture" width={300} />
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/vikramsingh159')
+    return response.json()
+}
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout/Layout.jsx'
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
+import User from './components/User/User';
+import Github, { githubInfoLoader } from './components/Github/Github';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route loader={githubInfoLoader} path='github'
+        element={<Github />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
